cc04357b3e3e09b5b7ae010e5fe0b0411493f73e
ekoenitz <33014529+ekoenitz@users.noreply.github.com>
2018-02-20 20:42:53 -0800

    Checking the "highlight paragraphs" box will now disable sentence-based highlighting.

--- src/sbd.js
+++ src/sbd.js
@@ -4,9 +4,9 @@
 ********/
 if (sessionStorage["postAutorunCheck"] != "true") {
   chrome.storage.local.get('autorun', function (storageObject) {
-     if (storageObject.autorun) {
+    if (storageObject.autorun) {
       highlight();
-     }
+    }
   });
   sessionStorage.setItem("postAutorunCheck", "true");
 }
@@ -14,24 +14,35 @@ else {
   highlight();
 }
 
-function highlight() {
+function handleSentences(paragraph, hue) {
   let tokenizer = require('sbd');
+  let sentences = tokenizer.sentences(paragraph.innerHTML);
+  paragraph.innerHTML = '';
+  
+  for (let j = 0; j < sentences.length; j++) {
+    let line = document.createElement('span');
+    line.style.backgroundColor = 'hsl(' + hue + ', 75%, 97.5%)';
+    line.style.boxDecorationBreak = 'clone';
+    line.style.webkitBoxDecorationBreak = 'clone';
+    hue = (hue + 130) % 360;
+    line.innerHTML = sentences[j] + ' ';
+    paragraph.appendChild(line);
+  }
+}
+
+function highlight() {
   let paragraphs = document.getElementsByTagName('p');
   let hue = Math.floor(Math.random()*360);
 
   for (let i = 0; i < paragraphs.length; i++) {
-    let element = paragraphs[i];
-    let sentences = tokenizer.sentences(element.innerHTML);
-    element.innerHTML = '';
-    for (let j = 0; j < sentences.length; j++) {
-      let line = document.createElement('span');
-      line.style.backgroundColor = 'hsl(' + hue + ', 75%, 97.5%)';
-      line.style.boxDecorationBreak = 'clone';
-      line.style.webkitBoxDecorationBreak = 'clone';
-      hue = (hue + 130) % 360;
-      line.innerHTML = sentences[j] + ' ';
-      element.appendChild(line);
-    }
+    chrome.storage.local.get('paragraph', function (storageObject) {
+      if (storageObject.paragraph) {
+        console.log("Here!");
+      }
+      else {
+        handleSentences(paragraphs[i], hue);
+      }
+    });
   }
   sessionStorage.removeItem("postAutorunCheck");
 }
