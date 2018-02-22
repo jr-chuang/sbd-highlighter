0bcdcb014dd2b5bb227f8452e4d89c15ec424a70
Tariq Eibad Anees <tariq@dhcp-169-233-175-145.cruznet.ucsc.edu>
2018-02-22 09:35:56 -0800

    added the font size

--- src/script.js
+++ src/script.js
@@ -13,11 +13,32 @@ function runScript() {
   });
 }
 
+function fontSizeScript(fntValue) {
+  console.log('the font value is ' + fntValue);
+
+  chrome.tabs.executeScript({
+    code: 'var sbd_fontvalue =' + fntValue + ';'
+  })
+  chrome.tabs.executeScript({
+
+    file: 'fontchange.js'
+  });
+}
+
+
 document.addEventListener('DOMContentLoaded', () => {
   getCurrentTabUrl((url) => {
     var button = document.getElementById('enable');
     button.addEventListener('click', () => {
       runScript();
     });
+
+
+    var fontSizeBtn = document.getElementById('fntbtn');
+    fontSizeBtn.addEventListener('click', () => {
+      var fntValue = document.getElementById('fntSize').value;
+      fontSizeScript(fntValue);
+    });
+
   });
 });
